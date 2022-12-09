a6a4c92af06e1c7ac2e9ef179a1305ab318952aa
c0dejohn <jemanyoma07@gmail.com>
2022-12-09 10:39:41 -0500

    feat: add health check

--- src/email-handler/controller/email-handler.controller.ts
+++ src/email-handler/controller/email-handler.controller.ts
@@ -11,20 +11,21 @@ import { EmailHandlerService } from '../service/email-handler.service';
 import RequestBodyDto from '../dto/request-body.dto';
 import ResponseDto from '../dto/response.dto';
 
-@Controller('email-handler/api/v1/')
+@Controller('')
 export class EmailHandlerController {
   private readonly logger = new Logger(EmailHandlerController.name);
   constructor(private readonly emailHandlerService: EmailHandlerService) {}
 
-  @Post('send-email')
+  @Get('health')
+  @HttpCode(HttpStatus.OK)
+  async health(): Promise<object> {
+    return { status: 'OK' };
+  }
+
+  @Post('email-handler/api/v1/send-email')
   @HttpCode(HttpStatus.ACCEPTED)
   async sendEmail(@Body() body: RequestBodyDto): Promise<ResponseDto> {
     this.logger.log('EmailHandlerController@sendEmail entry data', body);
     return await this.emailHandlerService.sendEmail(body);
-  }
-  @Get('health')
-  @HttpCode(HttpStatus.OK)
-  async health(): Promise<string> {
-    return 'OK';
   }
 }
